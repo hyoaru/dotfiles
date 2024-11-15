5a19c750581cb94ae6aabb76ee6997b7110dc13c
hyoaru <jjcabreraaaa@gmail.com>
2024-11-15 17:02:48 +0800

    [fix] telescope no border

--- nvim/lua/plugins/fullerene.lua
+++ nvim/lua/plugins/fullerene.lua
@@ -1,3 +1,19 @@
 return {
-  {"steguiosaur/fullerene.nvim"}
+  {
+    "steguiosaur/fullerene.nvim",
+    config = function()
+      require("fullerene").setup({
+        styles = {
+          booleans = { italic = true, bold = true },
+        },
+        integrations = {
+          hop = true,
+          telescope = false,
+        },
+        highlight_overrides = {
+          Comment = { bg = "#ff0000" }
+        }
+      })
+    end
+  }
 }
