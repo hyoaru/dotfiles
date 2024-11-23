49c0d3cab4bb000b74a088799109d7f463147fbd
hyoaru <jjcabreraaaa@gmail.com>
2024-11-23 18:19:23 +0800

    [add] web plugins

---
+++ nvim/lua/plugins/web.lua
@@ -0,0 +1,12 @@
+return {
+  "neovim/nvim-lspconfig",
+  opts = {
+    servers = {
+      tailwindcss = {},
+      ruby_lsp = {},
+      emmet_language_server = {},
+      html = {},
+      solargraph = {},
+    },
+  },
+}
