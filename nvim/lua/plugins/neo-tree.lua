eb28c3d850c872f4f7b29efa7ae9edaa0517ad37
hyoaru <jjcabreraaaa@gmail.com>
2024-11-24 16:50:49 +0800

    [update] neotree config add show hidden files

---
+++ nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,15 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  opts = {
+    filesystem = {
+      filtered_items = {
+        visible = true,
+        hide_dotfiles = false,
+      },
+      follow_current_file = {
+        enabled = true,
+      },
+      group_empty_dirs = true,
+    },
+  },
+}
