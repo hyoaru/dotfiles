facaf757f6c9cfc77f0d4fc14b94b0dfedab47de
hyoaru <jjcabreraaaa@gmail.com>
2024-11-25 20:16:03 +0800

    [update] disable inline hints

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -19,6 +19,10 @@ require("lazy").setup({
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import any extra modules here
+    { import = "lazyvim.plugins.extras.coding.mini-surround" },
+    { import = "lazyvim.plugins.extras.lang.tailwind" },
+    { import = "lazyvim.plugins.extras.lang.typescript" },
+    { import = "lazyvim.plugins.extras.formatting.prettier" },
     -- import/override with your plugins
     { import = "plugins" },
   },
