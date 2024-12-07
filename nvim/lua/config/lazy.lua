fa2c3820500ff35d052e281dba62285144d4b9aa
hyoaru <jjcabreraaaa@gmail.com>
2024-12-07 12:45:58 +0800

    [add] supermaven

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -24,6 +24,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.lang.ruby" },
+    { import = "lazyvim.plugins.extras.ai.supermaven" },
     -- import/override with your plugins
     { import = "plugins" },
   },
