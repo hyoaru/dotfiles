73b99c314af91b63e7025f53c72eceef7e7545a7
hyoaru <jjcabreraaaa@gmail.com>
2024-12-30 00:19:21 +0800

    [fix] php file on open text grayed out

--- nvim/lua/plugins/laravel.lua
+++ nvim/lua/plugins/laravel.lua
@@ -71,13 +71,4 @@ return {
       lsp_server = "intelephense",
     },
   },
-  {
-    -- Add the blade-nav.nvim plugin which provides Goto File capabilities
-    -- for Blade files.
-    "ricardoramirezr/blade-nav.nvim",
-    dependencies = {
-      "hrsh7th/nvim-cmp",
-    },
-    ft = { "blade", "php" },
-  },
 }
