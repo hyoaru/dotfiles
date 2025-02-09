f997616d203a5ce238bb3decd0fb48b77d5940da
hyoaru <jjcabreraaaa@gmail.com>
2025-02-09 10:29:02 +0800

    [remove] version lock

--- commit.sh
+++ commit.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 if [ ! -d ~/.config/nvim ]; then
-    mkdir -p ~/.config/nvim
-    echo "Nvim directory created."
+  mkdir -p ~/.config/nvim
+  echo "Nvim directory created."
 fi
 
 yes | cp -rf ./nvim/* ~/.config/nvim
