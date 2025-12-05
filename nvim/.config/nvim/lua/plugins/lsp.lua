57b5b9321477c6f998076b3ff66d012b90335830
hyoaru <jjcabreraaaa@gmail.com>
2025-12-05 12:47:36 +0800

    Add go diagnostics muting

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,10 +1,25 @@
 return {
-  "neovim/nvim-lspconfig",
-  opts = {
-    servers = {
-      lua_ls = {
-        mason = false,
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        lua_ls = {
+          mason = false,
+        },
+        gopls = {
+          settings = {
+            gopls = {
+              hints = {
+                parameterNames = false,
+              },
+              analyses = {
+                ST1000 = false,
+              },
+            },
+          },
+        },
       },
     },
   },
+  {},
 }
